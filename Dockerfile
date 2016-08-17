a0b20b384be48a982fa348bd1a805acb3882f408
Ville Väänänen <ville.vaananen@lucify.com>
2016-08-18 01:49:52 +0300

    Try to register the service before starting ip

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM sameersbn/gitlab:8.10.2-1
 
 COPY app/models/ci /home/git/gitlab/app/models/ci
+COPY entrypoint.sh /sbin/entrypoint.sh
+RUN chmod 755 /sbin/entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+source ${GITLAB_RUNTIME_DIR}/functions
+
+[[ $DEBUG == true ]] && set -x
+
+case ${1} in
+  app:init|app:start|app:sanitize|app:rake)
+
+    initialize_system
+    configure_gitlab
+    configure_gitlab_shell
+    configure_nginx
+
+    case ${1} in
+      app:start)
+        migrate_database
+        rm -rf /var/run/supervisor.sock
+        REG_URL=http://${REGISTER_SERVICE}/${SERVICE_NAME:-gitlab}
+        if curl -X PUT -s --connect-timeout 1 $REG_URL; then
+          echo "Service registered with name ${SERVICE_NAME:-gitlab}"
+        else
+          echo "Service not registered"
+        fi
+        exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
+        ;;
+      app:init)
+        migrate_database
+        ;;
+      app:sanitize)
+        sanitize_datadir
+        ;;
+      app:rake)
+        shift 1
+        execute_raketask $@
+        ;;
+    esac
+    ;;
+  app:help)
+    echo "Available options:"
+    echo " app:start        - Starts the gitlab server (default)"
+    echo " app:init         - Initialize the gitlab server (e.g. create databases, compile assets), but don't start it."
+    echo " app:sanitize     - Fix repository/builds directory permissions."
+    echo " app:rake <task>  - Execute a rake task."
+    echo " app:help         - Displays the help"
+    echo " [command]        - Execute the specified command, eg. bash."
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
